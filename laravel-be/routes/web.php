e204b706cd5eea99b985e8f9cf75608b0f13b00f
Emiliano Carasa <emilianocarasa@gmail.com>
2020-04-18 17:11:03 -0300

    panel de inicio usuario y pista

--- laravel-be/routes/web.php
+++ laravel-be/routes/web.php
@@ -16,21 +16,17 @@ Route::get('/', 'HomeController@index')->name('home');
 //Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
-<<<<<<< HEAD
-Route::get('/basesycond', 'BasesController@index')->name('home');;
-
-Route::get('/pista', 'PistasController@index'); //// nombre en url y el nombre del controller.php
-=======
 
 // bases y condiciones
 Route::post('/basesycond', 'HomeController@show');
->>>>>>> 306ee99bcc13c9c47f18c2a610a91b5446add4a8
 
+//panel paticipante
+    //pistas
+    Route::get('/pista', 'PistasController@index')->name('home');
 
 //grupos
 Route::post('/grupos/crear', 'GruposController@crear')->name('crear');
 Route::post('/grupo/{codigoGrupo}', 'GruposController@show')->name('crear');
 
 //marketplace
-
 Route::get('/store', 'RegalosController@index');
