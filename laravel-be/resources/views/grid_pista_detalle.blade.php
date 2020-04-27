06aca254b995a13b5b3712b80e47769782248893
Emiliano Carasa <emilianocarasa@gmail.com>
2020-04-26 22:44:09 -0300

    CAMBIOs 2604

--- laravel-be/resources/views/grid_pista_detalle.blade.php
+++ laravel-be/resources/views/grid_pista_detalle.blade.php
@@ -39,7 +39,8 @@
               @foreach ($mensajes as $mensaje)
                 <!-- USER STATUS TITLE -->
                 <!-- /USER STATUS TITLE -->
-                <p class="user-status-title"><span class="bold">{{$mensaje->emisor}}</span></p>
+                <p class="user-status-title"><span class="bold">Destinatario: {{$mensaje->receptor}}</span></p>
+              
         
                 <!-- USER STATUS TAG -->
                 <p class="user-status-tag online">Grupo de juego: {{$mensaje->grupo}}</p>
@@ -73,8 +74,124 @@
               <!-- CHAT WIDGET SPEAKER -->
               <div class="chat-widget-speaker left">
                 <!-- CHAT WIDGET SPEAKER MESSAGE -->
-                <p class="chat-widget-speaker-message" style="color:white;background-color:#615dfa">{{$mensaje->mensaje}}</p>
+                <p class="chat-widget-speaker-message" style="color:white;background-color:#615dfa">Pista: {{$mensaje->mensaje}}</p>
+                <p class="chat-widget-speaker-message" style="color:white;background-color:#615dfa">Buenas Noticias! Tu amigo te dejó un regalo</p>
+                <p></p>
+                <div class="product-preview">        
+                    <figure class="product-preview-image liquid" style="background: url(&quot;https://www.infobae.com/new-resizer/5iHq3D_WRtu1vURNMoDp7WW4yw8=/750x0/filters:quality(100)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2019/08/18202233/papas-fritas-dupla.jpg&quot;) center center / cover no-repeat;">
+                      <img src="https://www.infobae.com/new-resizer/5iHq3D_WRtu1vURNMoDp7WW4yw8=/750x0/filters:quality(100)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2019/08/18202233/papas-fritas-dupla.jpg" alt="item-01" style="display: none;">
+                    </figure>
+                      
+                      <!-- /PRODUCT PREVIEW IMAGE -->
+                  
+                      <!-- PRODUCT PREVIEW INFO -->
+                      <div class="product-preview-info">
+                        <!-- TEXT STICKER -->
+                        <p class="text-sticker"><span class="highlighted">$</span> 250</p>
+                        <!-- /TEXT STICKER -->
+                  
+                        <!-- PRODUCT PREVIEW TITLE -->
+                        <p class="product-preview-title">2x1 Birra + papas bravas</p>
+                        <!-- /PRODUCT PREVIEW TITLE -->
+                  
+                        <!-- PRODUCT PREVIEW CATEGORY -->
+                        <p class="product-preview-category digital"><a href="marketplace-category.html">Cerveceria</a></p>
+                        <!-- /PRODUCT PREVIEW CATEGORY -->
+                  
+                        <!-- PRODUCT PREVIEW TEXT -->
+                        <p class="product-preview-text">amigo! Tomate una birra conmigo</p>
+
+                      
+                        <a mp-mode="dftl" target="_blank" style="width: 100%;" href="https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=46588395-ac4fe9f8-4427-4c9c-8ca1-a256e01ad59d" name="MP-payButton" class="button small twitch blue-ar-l-rn-none">Ver Voucher</a>
+                        <!-- /PRODUCT PREVIEW TEXT -->
+                      </div>
+                      <!-- /PRODUCT PREVIEW INFO -->
+                  
+                      <!-- PRODUCT PREVIEW META -->
+                      <div class="product-preview-meta">
+                        <!-- PRODUCT PREVIEW AUTHOR -->
+                        <div class="product-preview-author">
+                          <!-- PRODUCT PREVIEW AUTHOR IMAGE -->
+                          <a class="product-preview-author-image user-avatar micro no-border" href="profile-timeline.html">
+                            <!-- USER AVATAR CONTENT -->
+                            <div class="user-avatar-content">
+                              <!-- HEXAGON -->
+                              <div class="hexagon-image-18-20" data-src="img/avatar/01.jpg" style="width: 18px; height: 20px; position: relative;"><canvas width="18" height="20" style="position: absolute; top: 0px; left: 0px;"></canvas></div>
+                              <!-- /HEXAGON -->
+                            </div>
+                            <!-- /USER AVATAR CONTENT -->
+                          </a>
+                          <!-- /PRODUCT PREVIEW AUTHOR IMAGE -->
+                  
+                          <!-- PRODUCT PREVIEW AUTHOR TITLE -->
+                          <p class="product-preview-author-title">By</p>
+                          <!-- /PRODUCT PREVIEW AUTHOR TITLE -->
+                  
+                          <!-- PRODUCT PREVIEW AUTHOR TEXT -->
+                          <p class="product-preview-author-text"><a href="profile-timeline.html">Antares</a></p>
+                          <!-- /PRODUCT PREVIEW AUTHOR TEXT -->
+                        </div>
+                        <!-- /PRODUCT PREVIEW AUTHOR -->
+                  
+                        <!-- RATING LIST -->
+                        <div class="rating-list">
+                          <!-- RATING -->
+                          <div class="rating filled">
+                            <!-- RATING ICON -->
+                            <svg class="rating-icon icon-star">
+                              <use xlink:href="#svg-star"></use>
+                            </svg>
+                            <!-- /RATING ICON -->
+                          </div>
+                          <!-- /RATING -->
+                  
+                          <!-- RATING -->
+                          <div class="rating filled">
+                            <!-- RATING ICON -->
+                            <svg class="rating-icon icon-star">
+                              <use xlink:href="#svg-star"></use>
+                            </svg>
+                            <!-- /RATING ICON -->
+                          </div>
+                          <!-- /RATING -->
+                  
+                          <!-- RATING -->
+                          <div class="rating filled">
+                            <!-- RATING ICON -->
+                            <svg class="rating-icon icon-star">
+                              <use xlink:href="#svg-star"></use>
+                            </svg>
+                            <!-- /RATING ICON -->
+                          </div>
+                          <!-- /RATING -->
+                  
+                          <!-- RATING -->
+                          <div class="rating filled">
+                            <!-- RATING ICON -->
+                            <svg class="rating-icon icon-star">
+                              <use xlink:href="#svg-star"></use>
+                            </svg>
+                            <!-- /RATING ICON -->
+                          </div>
+                          <!-- /RATING -->
+                  
+                          <!-- RATING -->
+                          <div class="rating">
+                            <!-- RATING ICON -->
+                            <svg class="rating-icon icon-star">
+                              <use xlink:href="#svg-star"></use>
+                            </svg>
+                            <!-- /RATING ICON -->
+                          </div>
+                          <!-- /RATING -->
+                        </div>
+                        <!-- /RATING LIST -->
+                      </div>
+                      <!-- /PRODUCT PREVIEW META -->
+                  </div>
                 <!-- /CHAT WIDGET SPEAKER MESSAGE -->
+
+
         
                 <!-- CHAT WIDGET SPEAKER TIMESTAMP -->
                 <p class="chat-widget-speaker-timestamp">10:05AM</p>
