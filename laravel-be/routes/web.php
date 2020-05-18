cf1b4f11e22bb4548a727b596dba034ac03081a7
Emiliano Carasa <emilianocarasa@gmail.com>
2020-05-18 18:42:45 -0300

    aa

--- laravel-be/routes/web.php
+++ laravel-be/routes/web.php
@@ -1,32 +1,24 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// home
+Route::get('/', 'RegalosController@index');
+Route::get('/home', 'HomeController@index');
 
 // bases y condiciones
 Route::get('/basesycond', 'HomeController@show');
 
-//panel paticipante
-    //pistas
-    Route::get('/pista', 'PistasController@index')->name('index');
-    Route::post('/pista/crear', 'PistasController@register')->name('register');
-    Route::get('/pista/enviados', 'PistasController@enviados')->name('enviados');
-    Route::get('/pista/recibidos', 'PistasController@recibidos')->name('recibidos');
-    Route::get('/pista/grupos', 'PistasController@grupos')->name('grupos');
-    Route::get('/pista/mensaje', 'PistasController@mensaje')->name('mensaje');
-    Route::get('/pista/mensaje/regalo', 'PistasController@mensajeregalo')->name('mensajeregalo');
-    Route::get('/pista/mensaje/regalo/modal', 'PistasController@modaldinamico')->name('modaldinamico');   
-    Route::get('/pista/grupodetalle', 'PistasController@grupodetalle')->name('grupodetalle');
-    Route::get('/pista/what', 'PistasController@enviarw')->name('enviarw');
-    Route::post('/pista/rp', 'PistasController@responder')->name('responder');
+//pistas
+Route::get('/pista', 'PistasController@index')->name('index');
+Route::post('/pista/crear', 'PistasController@register')->name('register');
+Route::get('/pista/enviados', 'PistasController@enviados')->name('enviados');
+Route::get('/pista/recibidos', 'PistasController@recibidos')->name('recibidos');
+Route::get('/pista/grupos', 'PistasController@grupos')->name('grupos');
+Route::get('/pista/mensaje', 'PistasController@mensaje')->name('mensaje');
+Route::get('/pista/mensaje/regalo', 'PistasController@mensajeregalo')->name('mensajeregalo');
+Route::get('/pista/mensaje/regalo/modal', 'PistasController@modaldinamico')->name('modaldinamico');   
+Route::get('/pista/grupodetalle', 'PistasController@grupodetalle')->name('grupodetalle');
+Route::get('/pista/what', 'PistasController@enviarw')->name('enviarw');
+Route::post('/pista/rp', 'PistasController@responder')->name('responder');
     
 //grupos
 Route::post('/grupos/crear', 'GruposController@crear')->name('crear');
@@ -35,19 +27,17 @@ Route::get('/grupo/sortear', 'GruposController@sortear')->name('sortear');
 Route::post('/grupo/integrantes/{codigoGrupo}', 'GruposController@integrantes')->name('integrantes');
 Route::post('/grupo/regalar', 'GruposController@regalar')->name('regalar');
 
-
 //marketplace
 Route::get('/store/categoria/{categoriaNombre}', 'RegalosController@busquedaCategoria');
 Route::get('/store', 'RegalosController@index');
-Route::get('/', 'RegalosController@index');
 
-
+// compras de cupones
 Route::get('/buy/{hash}/{CuponCode}/{estado}/success', 'RegalosController@success'); // estado 1
 Route::get('/buy/{hash}/{CuponCode}/{estado}/pendind', 'RegalosController@pending'); // estado 2
 
-Route::get('/home', 'HomeController@index');
-
+// cuenta
 Route::get('/cuenta', 'UserController@cuenta');
 Route::post('/cuenta/edit', 'UserController@updateProfile');
 
+//authenticacion
 Auth::routes();
