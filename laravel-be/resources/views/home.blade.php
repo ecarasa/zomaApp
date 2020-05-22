ad0fd9d67f4c218dadaf427f5026b2d412b670ed
Emiliano Carasa <emilianocarasa@gmail.com>
2020-05-22 10:10:29 -0300

    landing

--- laravel-be/resources/views/home.blade.php
+++ laravel-be/resources/views/home.blade.php
@@ -1,6 +1,221 @@
-<h1>Home</h1>
 
-<form method="post" action="/logout">
-@csrf
-<button type="submit" class="button medium secondary" style="width: 100%;">cerrr session</a>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- styles -->
+  <link rel="stylesheet" href="{{ env('APP_URL_PUERTO') }}/css/styleshome.min.css">
+  <!-- favicon -->
+  <link rel="icon" href="img/favicon.ico">
+  <title>CheAmigo | Mas que un amigo invisible</title>
+</head>
+<body>
+    <style>
+        #top {
+            background: url(../img/landing/landing-background.jpg) no-repeat 0!important;
+        }
+        .architecture-item-list .architecture-item {
+            display: -ms-flexbox;
+            display: flex;
+            -ms-flex-pack: center;
+            justify-content: center;
+            width: 100%;
+            height: 165px!important;
+            position: relative;
+        }
+        </style>
+  <!-- NAVIGATION WRAP -->
+  <div class="navigation-wrap">
+    <!-- NAVIGATION -->
+    <nav class="navigation grid-limit">
+      <!-- LOGO -->
+      <div class="logo void">
+        <!-- LOGO ICON -->
+        
+        <!-- /LOGO ICON -->
+      </div>
+      <!-- /LOGO -->
+
+
+      <!-- BUTTON -->
+      <a class="button small primary" href="#" target="_blank">Crear Cuenta</a>
+      <!-- /BUTTON -->
+    </nav>
+    <!-- /NAVIGATION -->
+  </div>
+  <!-- /NAVIGATION WRAP -->
+
+  <!-- BANNER WRAP -->
+  <div id="top" class="banner-wrap">
+    <!-- BANNER -->
+    <div class="banner grid-limit">
+      <!-- BANNER INFO -->
+      <div class="banner-info">
+        <!-- BANNER IMAGE -->
+        
+        <!-- /BANNER IMAGE -->
+
+        <!-- BANNER PRETITLE -->
+        <p class="banner-pretitle">Bienvenidos a</p>
+        <!-- /BANNER PRETITLE -->
+
+        <!-- BANNER TITLE -->
+        <p class="banner-title">Che Amigo</p>
+        <!-- /BANNER TITLE -->
+
+        <!-- BANNER TEXT -->
+        <p class="banner-text">Es momento de estar cerca, aun estando lejos. Por eso te invitamos a armar un amigo invisible y que puedas, atraves de "cheamigo.com.ar" enviar regalos o pistas a tu amigo invisbible. Que estas esperando ?</p>
+        <!-- /BANNER TEXT -->
+
+        <!-- BUTTON -->
+        <a class="button" href="/grupos/crear">Nuevo Juego</a>
+        <!-- /BUTTON -->
+      </div>
+      <!-- /BANNER INFO -->
+    </div>
+    <!-- /BANNER -->
+  </div>
+  <!-- /BANNER WRAP -->
+
+  <!-- ARCHITECTURE ITEM LIST -->
+  <div class="architecture-item-list">
+    <!-- ARCHITECTURE ITEM -->
+    <div class="architecture-item">
+      <!-- ARCHITECTURE IMAGE -->
+      <img class="architecture-image" src="{{ env('APP_URL_PUERTO') }}/img/regalo.png" alt="feature-html" style="width: 36%;">
+      <!-- /ARCHITECTURE IMAGE -->
+
+      <!-- ARCHITECTURE IMAGE -->
+      <img class="architecture-image-hover" src="{{ env('APP_URL_PUERTO') }}/img/regalo.png" alt="feature-html-hover" style="width: 36%;">
+      <!-- /ARCHITECTURE IMAGE -->
+    </div>
+    <!-- /ARCHITECTURE ITEM -->
+
+    <!-- ARCHITECTURE ITEM -->
+    <div class="architecture-item">
+      <!-- ARCHITECTURE IMAGE -->
+      <img class="architecture-image" src="{{ env('APP_URL_PUERTO') }}/img/compuregalo.png" alt="feature-html-hover" style="width: 36%;    margin-top: 20px;">
+      <!-- /ARCHITECTURE IMAGE -->
+
+      <!-- ARCHITECTURE IMAGE -->
+      <img class="architecture-image-hover" src="{{ env('APP_URL_PUERTO') }}/img/compuregalo.png" alt="feature-html-hover" style="width: 36%;    margin-top: 20px;">
+      <!-- /ARCHITECTURE IMAGE -->
+    </div>
+    <!-- /ARCHITECTURE ITEM -->
+
+    <!-- ARCHITECTURE ITEM -->
+    <div class="architecture-item">
+      <!-- ARCHITECTURE IMAGE -->
+      <img class="architecture-image" src="{{ env('APP_URL_PUERTO') }}/img/regalos_mano.png" alt="feature-html-hover" style="width: 36%;">
+      <!-- /ARCHITECTURE IMAGE -->
+
+      <!-- ARCHITECTURE IMAGE -->
+      <img class="architecture-image-hover" src="{{ env('APP_URL_PUERTO') }}/img/regalos_mano.png" alt="feature-html-hover" style="width: 36%;">
+      <!-- /ARCHITECTURE IMAGE -->
+    </div>
+    <!-- /ARCHITECTURE ITEM -->
+
+
+  </div>
+  <!-- /ARCHITECTURE ITEM LIST -->
+
+
+
+  <!-- SECTION WRAP -->
+  <div id="features" class="section-wrap">
+    <!-- SECTION -->
+    <section class="section grid-limit">
+      <!-- SECTION INFO -->
+      <div class="section-info">
+        <!-- SECTION PRETITLE -->
+        <p class="section-pretitle">CHE AMIGO</p>
+        <!-- /SECTION PRETITLE -->
+
+        <!-- SECTION TITLE -->
+        <h2 class="section-title">¿Como funciona esto?</h2>
+        <!-- /SECTION TITLE -->
+
+        <!-- SECTION TEXT -->
+        
+        <!-- /SECTION TEXT -->
+      </div>
+      <!-- /SECTION INFO -->
+
+      <!-- GRID -->
+      <div class="grid grid-4">
+        <!-- FEATURE ITEM -->
+        <article class="feature-item">
+          <h2 class="feature-item-title">1.</h2>
+          <!-- /FEATURE ITEM TITLE -->
+
+          <!-- FEATURE ITEM TEXT -->
+          <p class="feature-item-text">Crea tu cuenta e invita a tus amigos, no hay limite, ni hay que pagar nada. Totalmente GRATIS!.</p>
+          <!-- /FEATURE ITEM TEXT -->
+        </article>
+        <!-- /FEATURE ITEM -->
+
+        <!-- FEATURE ITEM -->
+        <article class="feature-item">
+          <!-- FEATURE ITEM IMAGE -->
+          
+          <!-- /FEATURE ITEM IMAGE -->
+
+          <!-- FEATURE ITEM TITLE -->
+          <h2 class="feature-item-title">2.</h2>
+          <!-- /FEATURE ITEM TITLE -->
+
+          <!-- FEATURE ITEM TEXT -->
+          <p class="feature-item-text">Cuando todos se hayan unido, ingresa al grupo y sortealo, y descubri tu amigo invisible.</p>
+          <!-- /FEATURE ITEM TEXT -->
+        </article>
+        <!-- /FEATURE ITEM -->
+
+        <!-- FEATURE ITEM -->
+        <article class="feature-item">
+          <!-- FEATURE ITEM IMAGE -->
+          
+          <!-- /FEATURE ITEM IMAGE -->
+
+          <!-- FEATURE ITEM TITLE -->
+          <h2 class="feature-item-title">3.</h2>
+          <!-- /FEATURE ITEM TITLE -->
+
+          <!-- FEATURE ITEM TEXT -->
+          <p class="feature-item-text">Envíale pistas y regalos, y ademas de ayudar a los comercios que debieron cerrar sus puertas por el Covid19, estas mas cerca de tus amigos.</p>
+          <!-- /FEATURE ITEM TEXT -->
+        </article>
+        <!-- /FEATURE ITEM -->
+
+    
+    
+      </div>
+      <!-- /GRID -->
+    </section>
+    <!-- /SECTION -->
+  </div>
+  <!-- /SECTION WRAP -->
+
+  <!-- SECTION WRAP -->
+  <div class="section-wrap">
+    <!-- SECTION -->
+    <section class="section grid-limit">
+      <!-- SECTION INFO -->
+      <div class="section-info">
+        
+        <h2 class="section-title">
+       <a href="/grupos/crear"> >>     A JUGAR!</a>
+        </h2>
+
+      </div>
+    </section>
+    <!-- /SECTION -->
+  </div>
+  <!-- /SECTION WRAP -->
+
+  
+
+<!-- app -->
+<script src="{{ env('APP_URL_PUERTO') }}/app.bundle.min.js"></script>
+</body>
+</html>
